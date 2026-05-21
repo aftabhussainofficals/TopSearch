205d831d39b12b4f7250340eeb2cab0dc9256cba
Aftab <aftabhussain.dev@gmail.com>
2026-05-21 18:49:27 +0500

    cmake added

---
+++ build/CMakeFiles/TopSearch.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/DELL/Desktop/TopSearch/PlatformAPIs.cpp" "CMakeFiles/TopSearch.dir/PlatformAPIs.cpp.obj" "gcc" "CMakeFiles/TopSearch.dir/PlatformAPIs.cpp.obj.d"
+  "C:/Users/DELL/Desktop/TopSearch/SearchEngine.cpp" "CMakeFiles/TopSearch.dir/SearchEngine.cpp.obj" "gcc" "CMakeFiles/TopSearch.dir/SearchEngine.cpp.obj.d"
+  "C:/Users/DELL/Desktop/TopSearch/main.cpp" "CMakeFiles/TopSearch.dir/main.cpp.obj" "gcc" "CMakeFiles/TopSearch.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/TopSearch.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TopSearch.dir/PlatformAPIs.cpp.obj"
+  "CMakeFiles/TopSearch.dir/PlatformAPIs.cpp.obj.d"
+  "CMakeFiles/TopSearch.dir/SearchEngine.cpp.obj"
+  "CMakeFiles/TopSearch.dir/SearchEngine.cpp.obj.d"
+  "CMakeFiles/TopSearch.dir/main.cpp.obj"
+  "CMakeFiles/TopSearch.dir/main.cpp.obj.d"
+  "TopSearch.exe"
+  "TopSearch.exe.manifest"
+  "TopSearch.pdb"
+  "libTopSearch.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TopSearch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
